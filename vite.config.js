0dcb05dc0266dc4919097b48c503d25ff0fcb66e
kaian-moura <193531568+Kaian-Moura@users.noreply.github.com>
2025-11-24 15:52:03 -0300

    fix: corrige erro de depoloy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/kameka/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: false,
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
+export default defineConfig(({ command }) => {
+  const base = command === "serve" ? "/" : "/kameka/";
+
+  return {
+    base,
+    build: {
+      outDir: "dist",
+      assetsDir: "assets",
+      sourcemap: false,
+    },
+    server: {
+      port: 3000,
+      open: true,
+    },
+  };
 });
